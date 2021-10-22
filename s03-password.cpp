c7819e5c6bdb0de6bc97c096aa9b7366364b1c14
MarcinGajkowski <91730962+MarcinGajkowski@users.noreply.github.com>
2021-10-22 15:08:16 +0200

    Add files via upload

--- s03-password.cpp
+++ s03-password.cpp
@@ -10,7 +10,7 @@ auto main(int argc, char* argv[]) -> int
 auto ask_user_for_password(std::string prompt) -> int
 
     
-    std::cout << prompt << " int:";
+    std::cout << prompt << "int: ";
     auto password = std::string{argv[1]};
     std::getline(std::cin, password);
     return std::stoi(password);
