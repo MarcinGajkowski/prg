03136d9e96a2aff894469e921e468c8e7ede5307
MarcinGajkowski <91730962+MarcinGajkowski@users.noreply.github.com>
2021-11-05 13:40:39 +0100

    Update s03-guessing-game.cpp

--- s03-guessing-game.cpp
+++ s03-guessing-game.cpp
@@ -7,26 +7,38 @@ std::uniform_int_distribution < int > d100 (1 , 100);
 
 auto const randoe = d100(rd);
 
-auto main(int argc, char* argv[]) -> int
-    {
-        auto ask_user_for_guess(std::stoi prompt) -> int
-            {
-            std::cout << prompt << "guess: ";
-            auto password = std::stoi(argv[1]);
-            std::getline(std::cin, guess);
-            return 0;
-            }
 
+auto ask_user_for_guess(std::string prompt) -> int
+{
+std::cout << prompt << "guess: ";
+auto answer = std::string{};
+std::getline(std::cin, answer);
+return std::stoi(answer);
+}
+
+    auto main() -> int
+    {
+        auto const guess = ask_user_for_guess(" ");
         do
         {
-        if (randoe > guess) 
+        {
+        ask_user_for_guess(" ");
+        }
+        if (randoe < guess) 
             {
             std::cout << "too big!\n";
-            }
-        else if (randoe < guess) 
+            } 
+        else if (randoe > guess) 
             {
             std::cout << "too small!\n";
             }
         }
         while (randoe != guess);
+
+        if (randoe == guess)
+            {
+            std::cout << "just right!\n";
+            }
+        return 0;
     }
+
