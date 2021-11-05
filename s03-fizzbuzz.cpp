b9d8c2a7aa1bd1106dc45c1d7f5eed6e114ffb31
MarcinGajkowski <91730962+MarcinGajkowski@users.noreply.github.com>
2021-11-05 14:18:20 +0100

    Add files via upload

---
+++ s03-fizzbuzz.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+auto main(int argc, char* argv[]) -> int
+{
+    if (argc == 0) 
+        {
+        return 1;
+        }
+
+    auto const n = std::stoi(argv[1]);
+    for (auto i = 1; i <= n; ++i) 
+        {
+        if (i % 3 == 0) 
+        {
+        std::cout << i << " Fizz\n";
+
+            if (i % 5 == 0) 
+            {
+            std::cout << i << " FizzBuzz\n";
+            }
+        }
+
+        else if (i % 5 == 0) 
+        {
+        std::cout << i << " Buzz\n";
+        }
+
+        else 
+        {
+        std::cout << i << "\n";
+        }
+        }
+    return 0;
+}
