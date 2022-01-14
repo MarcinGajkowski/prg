fc239b1f5d8798a27585f10f556216192d1bbe1a
MarcinGajkowski <91730962+MarcinGajkowski@users.noreply.github.com>
2022-01-14 14:59:56 +0100

    uhhhh

--- s03-password.cpp
+++ s03-password.cpp
@@ -1,42 +1,40 @@
 # include <iostream>
 # include <string>
 
-auto ask_user_for_password(std::string prompt) -> int
+auto ask_user_for_password(std::string prompt) -> void
 {
     std::cout << prompt << "password: ";
     auto password = std::string{};
     std::getline(std::cin, password);
-    return 0;
 }
-    auto main(int argc, char* argv[]) -> int
+    
+auto main(int argc, char* argv[]) -> int
+{
+    auto const password = std::string{argv[1]};
+
+    if (argc > 1)
     {
-        if (argc == 0) 
+        do
+        {
+            ask_user_for_password(" ");
+            if (password == "student")
             {
-            return 1;
+                std::cout << "ok!\n";
+                return 0;
             }
+        }
+        while (password != "student");
 
-        auto const password = std::string{argv[1]};
-
-        if (password != "student")
-            {
-               do
-                {
-                ask_user_for_password(" ");
-
-                if (password == "student")
-                   {
-                   std::cout << "ok!\n";
-                   }
-                }
-               while (password != "student");
-
-            return 0;
-            }
-            
-        else 
-            {
+        if (password == "student")
+        {
             std::cout << "ok!\n";
-            }
-    return 0;
+            return 0;
+        }    
     }
-
+    else if (argc == 1)
+    {
+        std::cout << "No password detected. Please try again.\n";
+    } 
+            
+        return 0;
+}
