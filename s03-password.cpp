4bf3ff387e429acc97806cbeed44e4df9ccf8b66
MarcinGajkowski <91730962+MarcinGajkowski@users.noreply.github.com>
2022-01-18 09:00:36 +0100

    już lepiej (i prościej) (nienawidzę tego)

--- s03-password.cpp
+++ s03-password.cpp
@@ -1,49 +1,20 @@
 #include <iostream>
 #include <string>
 
-auto ask_user_for_password() -> void
-{
-    std::cout << "password: ";
-    auto password = std::string{};
-    std::getline(std::cin, password);
-    if (password == "student")
+auto main() -> int
+{ 
+    while (true)
     {
-        std::cout << "ok!\n";
-    } 
-}
-    
-auto main(int argc, char* argv[]) -> int
-{
-    auto const password = std::string{argv[1]};
-
-    if (argc > 1)
-    {
+    	auto password = std::string{};
+    	std::cout << "password: ";
+    	std::getline(std::cin, password);
+    	
     	if (password == "student")
     	{
-        	std::cout << "ok!\n";
-        	return 0;
-    	}  
-    	
-    	while (password != "student") // nie wiem jak uciąć tą pętlę
-    	{
-        	do
-        	{
-            	ask_user_for_password();
-        	}
-        	while (password != "student");
-        	
-        	if (password == "student")
-    		{
-        		std::cout << "ok!\n";
-        		break;
-    		}
+    		std::cout << "ok!\n";
     		break;
-        }
-        
-        return 0; 
+    	}
     }
-    else if (argc == 1)
-    {
-        std::cout << "No password detected. Please try again.\n";
-    }   
+    
+	return 0;
 }
