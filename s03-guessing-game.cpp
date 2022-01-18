0ae031743a80e61410e407235deebd5df4e5aaa7
MarcinGajkowski <91730962+MarcinGajkowski@users.noreply.github.com>
2022-01-18 20:12:04 +0100

    lepszy rng

--- s03-guessing-game.cpp
+++ s03-guessing-game.cpp
@@ -1,44 +1,35 @@
 #include <iostream>
 #include <string>
-#include <random>
+#include <ctime>
 
-std::random_device rd;
-std::uniform_int_distribution < int > d100 (1 , 100);
+int randoe;
+int guess;
 
-auto const randoe = d100(rd);
-
-
-auto ask_user_for_guess(std::string prompt) -> int
+auto main() -> int
 {
-std::cout << prompt << "guess: ";
-auto answer = std::string{};
-std::getline(std::cin, answer);
-return std::stoi(answer);
-}
-
-    auto main() -> int
-    {
-        auto const guess = ask_user_for_guess(" ");
-        do
-        {
-        {
-        ask_user_for_guess(" ");
-        }
+	srand(time(0));
+	randoe = 1 + rand() % 100;
+		
+	while (true)
+	{
+		std::cout << "\n";
+		std::cout << "guess: ";
+		std::cin >> guess;
+		
         if (randoe < guess) 
-            {
+        {
             std::cout << "too big!\n";
-            } 
+        } 
         else if (randoe > guess) 
-            {
+        {
             std::cout << "too small!\n";
-            }
         }
-        while (randoe != guess);
-
         if (randoe == guess)
-            {
+        {
             std::cout << "just right!\n";
-            }
-        return 0;
-    }
-
+            break;
+        }
+	}	
+    
+    return 0;
+}
