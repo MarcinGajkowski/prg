44f69ec93e810e8f27a16c301edc9be32cbc4745
MarcinGajkowski <91730962+MarcinGajkowski@users.noreply.github.com>
2022-01-18 17:26:20 +0100

    update wizualny

--- s03-fizzbuzz.cpp
+++ s03-fizzbuzz.cpp
@@ -4,32 +4,32 @@
 auto main(int argc, char* argv[]) -> int
 {
     if (argc == 0) 
-        {
+    {
         return 1;
-        }
+    }
 
     auto const n = std::stoi(argv[1]);
     for (auto i = 1; i <= n; ++i) 
-        {
+    {
         if (i % 3 == 0) 
         {
-        std::cout << i << " Fizz\n";
-
+        	std::cout << i << " Fizz\n";
+        	
             if (i % 5 == 0) 
             {
-            std::cout << i << " FizzBuzz\n";
+            	std::cout << i << " FizzBuzz\n";
             }
         }
-
         else if (i % 5 == 0) 
         {
-        std::cout << i << " Buzz\n";
+        	std::cout << i << " Buzz\n";
         }
-
         else 
         {
-        std::cout << i << "\n";
+        	std::cout << i << "\n";
         }
-        }
+        
+    }
+    
     return 0;
 }
